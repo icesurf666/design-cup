61fb5e72a755be3c728ee097608a262896b7b032
Pavel <icesurf@yandex.ru>
2019-03-10 21:48:09 +0700

    initial commit

---
+++ js/common.js
@@ -0,0 +1,16 @@
+
+
+    $('.slider-blocks').slick({
+        dots: false,
+        infinite: true,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 1,
+        variableWidth: true
+    });
+    $(".slider-arrows__left").click(function() {
+        $('.slider-blocks').slick("prev");
+    });
+    $(".slider-arrows__right").click(function() {
+        $('.slider-blocks').slick("next");
+    });
